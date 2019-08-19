6c02e8b2f15a48ac95cabd4225231be566aa1f65
暮光小猿wzt(PC) <1142305747@qq.com>
2019-08-19 21:20:11 +0800

    nGender

---
+++ ngender.php
@@ -0,0 +1,82 @@
+<?php
+//检查是否有参数
+if (!isset($_GET['name'])) {
+    die('没有指定name参数');
+}
+//取名字
+$name = $_GET['name'];
+//打开文件
+$fhandle = fopen('charfreq.csv','r');
+//跳过第一行
+fgets($fhandle);
+//读数据库
+$total = 0;
+$total_male = 0;
+$total_female = 0;
+$freq = array();
+while (!feof($fhandle)) {
+    $line = explode(',', fgets($fhandle));
+    if (count($line) == 3) {
+        $total_male += intval($line[1]);
+        $total_female += intval($line[2]);
+        $freq[$line[0]] = array(floatval($line[2]), floatval($line[1]));
+    }
+}
+$total = $total_male + $total_female;
+foreach ($freq as $char=>$value) {
+    $freq[$char] = array(1.0 * $value[0] / $total_female, 1.0 * $value[1] / $total_male);
+}
+
+//计算一个性别的概率
+function probForGender($firstname, $gender) {
+    global $total_male;
+    global $total_female;
+    global $total;
+    global $freq;
+    $p;
+    if ($gender == 0) {
+        $p = 1.0 * $total_female / $total;
+    } else {
+        $p = 1.0 * $total_male / $total;
+    }
+    //遍历汉字字符串
+    preg_match_all('/[\x{4e00}-\x{9fa5}]/u', $firstname, $chinese);
+    preg_match_all('/[^\x{4e00}-\x{9fa5}]/u', $firstname, $string);
+    $result = array_merge(current($chinese), current($string));
+    foreach ($result as $word) {
+        //直接忽略非汉字字符
+        if (preg_match('/^[\x{4e00}-\x{9fa5}]$/u', $word)) {
+            if (array_key_exists($word, $freq)) {
+                $p *= $freq[$word][$gender];
+            }
+        }
+    }
+    return $p;
+}
+
+//开始计算
+$firstname = '';
+$firstFlag = true;
+//遍历汉字字符串
+preg_match_all('/[\x{4e00}-\x{9fa5}]/u', $name, $chinese);
+preg_match_all('/[^\x{4e00}-\x{9fa5}]/u', $name, $string);
+$result = array_merge(current($chinese), current($string));
+foreach ($result as $word) {
+    //直接忽略非汉字字符
+    if (preg_match('/^[\x{4e00}-\x{9fa5}]$/u', $word)) {
+        //去掉第一个字
+        if ($firstFlag) {
+            $firstFlag = false;
+            continue;
+        }
+        $firstname = $firstname . $word;
+    }
+}
+$pFemale = probForGender($firstname, 0);
+$pMale = probForGender($firstname, 1);
+if ($pMale > $pFemale) {
+    echo '{"result": ["male", ' . (1.0 * $pMale / ($pMale + $pFemale)) . "]}";
+} else {
+    echo '{"result": ["female", ' . (1.0 * $pFemale / ($pMale + $pFemale)) . "]}";
+}
+?>
---
+++ readme.md
@@ -0,0 +1,18 @@
+# NGender PHP
+
+这是[https://github.com/observerss/ngender](https://github.com/observerss/ngender)的PHP版本.
+
+## Usage
+
+复制两个文件 (ngender.php & charfreq.csv) 到一个支持PHP的网站.
+用HTTP/GET请求网站, 参数是"name=xxx", 网站内容是json格式, 可以被其它程序解析.
+
+## Example
+
+```
+http://localhost/ngender.php?name=赵本山
+{"result": ["male", 0.9836229687547]}
+
+http://localhost/ngender.php?name=宋丹丹
+{"result": ["female", 0.97594861289499]}
+```
